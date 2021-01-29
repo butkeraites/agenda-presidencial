b5efd0eb03fc17486f9f84870113d533b414d67e
Renan Brito Butkeraites <rbritobut?@gmail.com>
2021-01-28 21:02:46 -0300

    Criado V0 dashboard.

---
+++ agenda-presidencial/code/calendario.py
@@ -0,0 +1,112 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+from bs4 import BeautifulSoup
+from datetime import date, datetime, timedelta
+
+
+def get_all_dates(year, month, day):
+    sdate = date(year, month, day)   # start date
+    edate = date.today()   # end date
+    delta = edate - sdate       # as timedelta
+    all_dates = []
+    for i in range(delta.days + 1):
+        day = sdate + timedelta(days=i)
+        all_dates.append(str(day))
+
+    return all_dates
+
+def prepare_calls(year, month, day):
+    calls = {}
+    all_dates = get_all_dates(year, month, day)
+    for date in all_dates:
+        url = 'https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/agenda-do-presidente-da-republica/' + date
+        calls[date] = url
+    return calls
+
+def get_all_compromises_from_date(url, campos_de_interesse):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Compromissos do dia
+    compromissos = []
+    for campo in campos_de_interesse:
+        elementos_campo = soup.find_all(class_=campo)
+        for elemento_campo in elementos_campo:
+            compromisso = {}
+            for atributo in campos_de_interesse[campo]:
+                valor_atributo = elemento_campo.find(class_=atributo)
+                if valor_atributo:
+                    compromisso[atributo] = valor_atributo.string
+            compromissos.append(compromisso)
+    return compromissos
+
+def get_all_compromises(year, month, day, campos_de_interesse):
+    calls = prepare_calls(year, month, day)
+    compromises = {}
+    for call in calls:
+        print('Capturando ' + call + '[...]')
+        compromises[call] = get_all_compromises_from_date(calls[call], campos_de_interesse)
+    return compromises
+
+def has_all_data(meeting, campos_de_interesse):
+    all_data_found = True
+    for data in campos_de_interesse['item-compromisso']:
+        if data not in meeting:
+            all_data_found = False
+    return all_data_found
+
+def transform_compromises_in_dataframe(year, month, day, initial_index):
+    index = initial_index
+    
+    df_compromises = {
+        'MEETING_ID' : [],
+        'BEGIN_HOUR' : [],
+        'END_HOUR' : [],
+        'MEETING_TITLE' : [],
+        'MEETING_LOCATION' : []
+    }
+    
+    campos_de_interesse = {
+        'item-compromisso' : [
+            'compromisso-inicio',
+            'compromisso-fim',
+            'compromisso-titulo',
+            'compromisso-local'
+        ]
+    }
+
+    compromises = get_all_compromises(year, month, day, campos_de_interesse)
+    
+    for dates in compromises:
+        print('Incluindo no DataFrame [' + dates + ']')
+        for meeting in compromises[dates]:
+            if has_all_data(meeting, campos_de_interesse):
+                index += 1
+                df_compromises['MEETING_ID'].append(index)
+                df_compromises['BEGIN_HOUR'].append(datetime.strptime(dates + meeting['compromisso-inicio'], '%Y-%m-%d%Hh%M'))
+                df_compromises['END_HOUR'].append(datetime.strptime(dates + meeting['compromisso-fim'], '%Y-%m-%d%Hh%M'))
+                df_compromises['MEETING_TITLE'].append(meeting['compromisso-titulo'])
+                df_compromises['MEETING_LOCATION'].append(meeting['compromisso-local'])
+    
+    return pd.DataFrame(df_compromises)
+
+def get_max_id_and_max_date(engine):
+    result = pd.read_sql_query("SELECT MAX(A.MEETING_ID) MAX_ID, STRFTIME('%Y-%m-%d', MAX(A.BEGIN_HOUR)) MAX_DATE FROM AGENDA_PRESIDENCIAL AS A", engine)
+    parameters = {
+        'MAX_ID' : result['MAX_ID'][0],
+        'MAX_DATE' : datetime.strptime(result['MAX_DATE'][0], '%Y-%m-%d') + timedelta(days=1)
+    }
+    return parameters
+
+engine = create_engine('sqlite:////home/barbaruiva/Documents/Database/AGENDA_PRESIDENCIAL.db', echo=False)
+conn = engine.connect()
+
+df_calendario = pd.read_csv('/home/barbaruiva/Downloads/calendario.csv', sep=',')
+df_calendario.set_index('DATA', inplace=True)
+df_calendario.to_sql('CALENDARIO', con=engine, if_exists='append')
+
+# PRIMEIRA INCLUSAO DE REGISTROS NA BASE
+#df_compromises = transform_compromises_in_dataframe(2019, 1, 1, 0)
+#df_compromises.set_index('MEETING_ID', inplace=True)
+#df_compromises.to_sql('AGENDA_PRESIDENCIAL', con=engine, if_exists='append')
---
+++ agenda-presidencial/code/dashboard.py
@@ -0,0 +1,83 @@
+import pandas as pd
+pd.options.plotting.backend = "plotly"
+from sqlalchemy import create_engine
+from datetime import date, datetime, timedelta
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.express as px
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output
+
+def get_all_data():
+    engine = create_engine('sqlite:////home/barbaruiva/Documents/Database/AGENDA_PRESIDENCIAL.db', echo=False)
+    conn = engine.connect()
+    return pd.read_sql_query("SELECT * FROM AGENDA_PRESIDENCIAL", engine)
+
+def get_all_callendar():
+    engine = create_engine('sqlite:////home/barbaruiva/Documents/Database/AGENDA_PRESIDENCIAL.db', echo=False)
+    conn = engine.connect()
+    return pd.read_sql_query("SELECT * FROM CALENDARIO", engine)
+
+def get_meeting_date_and_duration(df):
+    df['BEGIN_HOUR'] = pd.to_datetime(df['BEGIN_HOUR'])
+    df['END_HOUR'] = pd.to_datetime(df['END_HOUR'])
+    df['MEETING_DATE'] = list(map(lambda x : x.date(), df['BEGIN_HOUR']))
+    df['MEETING_DURATION'] = list(map(lambda x : x.total_seconds()/3600,df['END_HOUR'] - df['BEGIN_HOUR']))
+    return df
+
+def get_callendar_types(df):
+    df['MEETING_DATE'] = pd.to_datetime(df['DATA'], dayfirst=True)
+    df['MEETING_DATE'] = list(map(lambda x : x.date(), df['MEETING_DATE']))
+    df['MES_REFERENCIA'] = pd.to_datetime(df['MES_REFERENCIA'], dayfirst=True)
+    df['MES_REFERENCIA'] = list(map(lambda x : x.date(), df['MES_REFERENCIA']))
+    df['FERIADO'] = pd.to_numeric(df['FERIADO'])
+    df['SEMANA_DO_ANO'] = pd.to_numeric(df['SEMANA_DO_ANO'])
+    df['MES'] = pd.to_numeric(df['MES'])
+    df['ANO'] = pd.to_numeric(df['ANO'])
+    return df
+
+df = get_all_data()
+df = get_meeting_date_and_duration(df)
+
+df_callendar = get_all_callendar()
+df_callendar = get_callendar_types(df_callendar)
+
+df_with_dates = df.merge(df_callendar, on='MEETING_DATE', how='left')
+df_duracao_por_data = df_with_dates.groupby('MES_REFERENCIA').sum()[['MEETING_DURATION']]
+df_duracao_por_data = df_duracao_por_data.reset_index()
+
+df_dia_uteis = df_callendar.groupby('MES_REFERENCIA').sum()[['DIA_UTIL']]
+df_dia_uteis['HORAS_DE_TRABALHO'] = df_dia_uteis['DIA_UTIL'] * 8
+df_dia_uteis = df_dia_uteis.reset_index()
+df_dia_uteis = df_dia_uteis.loc[df_dia_uteis['MES_REFERENCIA'] <= max(df_duracao_por_data['MES_REFERENCIA'])] 
+
+fig = px.bar(df_duracao_por_data,
+                x="MES_REFERENCIA",
+                y="MEETING_DURATION", 
+                barmode="group",
+                labels={
+                     "MES_REFERENCIA": "Mês de Referência",
+                     "MEETING_DURATION": "Horas de Atividades Oficiais"
+                 })
+
+fig.add_trace(go.Scatter(
+    x=df_dia_uteis["MES_REFERENCIA"],
+    y=df_dia_uteis["HORAS_DE_TRABALHO"],
+    name="Horas CLT"
+))
+
+
+# Initialise the app
+app = dash.Dash(__name__)
+
+# Define the app
+app.layout = html.Div(children=[
+    html.H1(children='Soma das horas de atividades oficiais por mês:'),
+    dcc.Graph(id='my-graph', figure = fig)
+], style={'width': '500'})
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)

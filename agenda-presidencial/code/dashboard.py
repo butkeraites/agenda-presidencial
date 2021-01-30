71e0fc076163cf414203d0f486fd38a5500a9591
Renan Brito Butkeraites <rbritobut?@gmail.com>
2021-01-30 17:26:29 -0300

    Tempo médio de trabalho diário + dia da semana.

--- agenda-presidencial/code/dashboard.py
+++ agenda-presidencial/code/dashboard.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 pd.options.plotting.backend = "plotly"
 from sqlalchemy import create_engine
@@ -16,6 +17,7 @@ from sklearn.decomposition import PCA
 from nltk.corpus import stopwords
 import nltk
 
+template='plotly_dark'
 
 def return_stop_words_portuguese():
     words = []
@@ -71,8 +73,7 @@ df_callendar = get_callendar_types(df_callendar)
 
 #Agrupado de horas de atividades oficiais por mes
 df_with_dates = df.merge(df_callendar, on='MEETING_DATE', how='left')
-df_duracao_por_data = df_with_dates.groupby('MES_REFERENCIA').sum()[['MEETING_DURATION']]
-df_duracao_por_data = df_duracao_por_data.reset_index()
+df_duracao_por_data = df_with_dates.groupby('MES_REFERENCIA').sum()[['MEETING_DURATION']].reset_index()
 
 #Horas de trabalho de uma pessoa com CLT
 df_dia_uteis = df_callendar.groupby('MES_REFERENCIA').sum()[['DIA_UTIL']]
@@ -87,11 +88,41 @@ agrupado_agenda_mensal = px.bar(df_duracao_por_data,
                 labels={
                      "MES_REFERENCIA": "Mês de Referência",
                      "MEETING_DURATION": "Horas de Atividades Oficiais"
-                 })
+                 }, 
+                 template=template)
 
 agrupado_agenda_mensal.add_trace(go.Scatter(
     x=df_dia_uteis["MES_REFERENCIA"],
     y=df_dia_uteis["HORAS_DE_TRABALHO"],
+    name="Horas CLT"
+))
+
+#Agrupado de horas de atividades oficiais por dia da semana
+ordem_dia_semana = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado"]
+df_duracao_por_dia_semana = df_with_dates.groupby('DIA_DA_SEMANA').sum()[['MEETING_DURATION']].reset_index()
+df_duracao_por_dia_semana['DIA_DA_SEMANA'] = pd.CategoricalIndex(df_duracao_por_dia_semana['DIA_DA_SEMANA'], ordered=True, categories=ordem_dia_semana)
+df_duracao_por_dia_semana = df_duracao_por_dia_semana.sort_values('DIA_DA_SEMANA')
+
+#Horas de trabalho de uma pessoa com CLT
+df_dia_uteis_semana = df_callendar.loc[df_callendar['MEETING_DATE'] <= max(df_with_dates['MEETING_DATE'])].groupby('DIA_DA_SEMANA').sum()[['DIA_UTIL']]
+df_dia_uteis_semana['HORAS_DE_TRABALHO'] = df_dia_uteis_semana['DIA_UTIL'] * 8
+df_dia_uteis_semana = df_dia_uteis_semana.reset_index()
+df_dia_uteis_semana['DIA_DA_SEMANA'] = pd.CategoricalIndex(df_dia_uteis_semana['DIA_DA_SEMANA'], ordered=True, categories=ordem_dia_semana)
+df_dia_uteis_semana = df_dia_uteis_semana.sort_values('DIA_DA_SEMANA')
+
+agrupado_agenda_semana = px.bar(df_duracao_por_dia_semana,
+                x="DIA_DA_SEMANA",
+                y="MEETING_DURATION", 
+                barmode="group",
+                labels={
+                     "DIA_DA_SEMANA": "Dia da Semana",
+                     "MEETING_DURATION": "Horas de Atividades Oficiais"
+                 },
+                 template=template)
+
+agrupado_agenda_semana.add_trace(go.Scatter(
+    x=df_dia_uteis_semana["DIA_DA_SEMANA"],
+    y=df_dia_uteis_semana["HORAS_DE_TRABALHO"],
     name="Horas CLT"
 ))
 
@@ -104,7 +135,8 @@ agrupado_agenda_hora_diaria = px.bar(df_atividades_por_hora,
                 labels={
                      "ROUNDED_BEGIN_HOUR": "Hora de Inicio",
                      "MEETING_ID": "Qtd. de Atividades Oficias"
-                 })
+                 }, 
+                 template=template)
 
 
 #Agrupado atividades oficiais por local
@@ -115,7 +147,8 @@ agrupado_agenda_local = px.treemap(df_atividades_por_local,
                 labels={
                      "MEETING_LOCATION": "Local de realização da atividade",
                      "MEETING_ID": "Qtd. de Atividades Oficias"
-                 })
+                 }, 
+                 template=template)
 
 #Examinando os tipos de reuniões que foram realizadas
 tf_idf_vec_smooth = TfidfVectorizer(use_idf=True,  
@@ -136,31 +169,62 @@ plot_por_similaridade = px.scatter(completed_df, x="DIMENSION_0", y="DIMENSION_1
                      "MEETING_LOCATION" : "Local da atividade",
                      "MEETING_DURATION" : "Duração da atividade",
                      "MEETING_TITLE" : "Descrição da Atividade"
-                 })
+                 }, 
+                 template=template)
+
+#Média de horas trabalhadas por dia
+media_geral_horas = df.groupby('MEETING_DATE').sum()[['MEETING_DURATION']].mean()
+media_geral = str(math.floor(media_geral_horas[0]))+'h'+str(math.floor((media_geral_horas[0] - math.floor(media_geral_horas[0]))*60))
+media_ultimos_30_dias_horas = df[df['MEETING_DATE'] > (date.today()- timedelta(days=30))].groupby('MEETING_DATE').sum()[['MEETING_DURATION']].mean()
+media_ultimos_30_dias = str(math.floor(media_ultimos_30_dias_horas[0]))+'h'+str(math.floor((media_ultimos_30_dias_horas[0] - math.floor(media_ultimos_30_dias_horas[0]))*60))
 
 # Initialise the app
-external_stylesheets = [dbc.themes.FLATLY]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
+app.config.suppress_callback_exceptions = True
 
 # Define the app
-app.layout = html.Div(children=[
-    html.H1(children='Analise da Agenda Presidencial'),
-    html.Div(className='grafico-de-barra-pequeno', children = [
-        html.H2('Quantidade de atividades oficiais por hora de início'),
-        dcc.Graph(id='agrupado-agenda-diaria', figure = agrupado_agenda_hora_diaria)
-    ]),
-    html.Div(className='grafico-de-barra-grande', children = [
-        html.H2('Horas de atividades oficiais por mês'),
-        dcc.Graph(id='agrupado-agenda-mensal', figure = agrupado_agenda_mensal)
-    ]),  
-    html.Div(className='diagrama-de-bloco', children = [
-        html.H2('Quantidade de atividades oficiais por local'),
-        dcc.Graph(id='agrupado-local', figure = agrupado_agenda_local)
-    ]),
-    html.Div(className='grafico-de-dispersao', children = [
-        html.H2('Atividades oficiais agrupadas por similaridade'),
-        dcc.Graph(id='agrupado-similaridade', figure = plot_por_similaridade)
-    ])
+app.layout = html.Div(className='row',
+    children=[
+        html.Div(className='two columns div-user-controls',
+                    children=[
+                        html.H2('Agenda Presidencial'),
+                        dbc.Card([
+                            dbc.CardHeader("Um típico dia de trabalho tem a duração de "),
+                            dbc.CardBody(
+                                [
+                                    html.H4(media_geral, className="card-title"),
+                                    html.P("desde a posse, ou", className="card-text"),
+                                    html.H4(media_ultimos_30_dias, className="card-title"),
+                                    html.P("nos ultimos 30 dias.", className="card-text"),
+                                ]
+                            )
+                        ],
+                        style={"width": "30rem"})
+                ]),
+        html.Div(className='eight columns div-for-charts bg-grey',
+                    children=[
+                        html.Div(className='grafico-de-barra-pequeno', children = [
+                            html.H2('Quantidade de atividades oficiais por hora de início'),
+                            dcc.Graph(id='agrupado-agenda-diaria', figure = agrupado_agenda_hora_diaria)
+                        ]),
+                        html.Div(className='grafico-de-barra-grande', children = [
+                            html.H2('Horas de atividades oficiais por mês'),
+                            dcc.Graph(id='agrupado-agenda-mensal', figure = agrupado_agenda_mensal)
+                        ]),
+                        html.Div(className='grafico-de-barra-grande', children = [
+                            html.H2('Horas de atividades oficiais por dia da semana'),
+                            dcc.Graph(id='agrupado-agenda-semana', figure = agrupado_agenda_semana)
+                        ]),  
+                        html.Div(className='diagrama-de-bloco', children = [
+                            html.H2('Quantidade de atividades oficiais por local'),
+                            dcc.Graph(id='agrupado-local', figure = agrupado_agenda_local)
+                        ]),
+                        html.Div(className='grafico-de-dispersao', children = [
+                            html.H2('Atividades oficiais agrupadas por similaridade'),
+                            dcc.Graph(id='agrupado-similaridade', figure = plot_por_similaridade)
+                        ])
+                    ])
+        
 ], style={'width': '500'})
 
 

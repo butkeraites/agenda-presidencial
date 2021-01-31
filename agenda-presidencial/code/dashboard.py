cf995430216bfd5a18ab671e4c5adac36556ed59
Renan Brito Butkeraites <rbritobut?@gmail.com>
2021-01-31 12:46:58 -0300

    Inclusão da visão acumulada + contagem Inicio/fim

--- agenda-presidencial/code/dashboard.py
+++ agenda-presidencial/code/dashboard.py
@@ -9,6 +9,7 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
@@ -48,6 +49,7 @@ def get_meeting_date_and_duration(df):
     df['BEGIN_HOUR'] = pd.to_datetime(df['BEGIN_HOUR'])
     df['ROUNDED_BEGIN_HOUR'] = list(map(lambda x : hour_rounder(x), df['BEGIN_HOUR']))
     df['END_HOUR'] = pd.to_datetime(df['END_HOUR'])
+    df['ROUNDED_END_HOUR'] = list(map(lambda x : hour_rounder(x), df['END_HOUR']))
     df['MEETING_DATE'] = list(map(lambda x : x.date(), df['BEGIN_HOUR']))
     df['MEETING_DURATION'] = list(map(lambda x : x.total_seconds()/3600,df['END_HOUR'] - df['BEGIN_HOUR']))
     return df
@@ -150,6 +152,41 @@ agrupado_agenda_local = px.treemap(df_atividades_por_local,
                  }, 
                  template=template)
 
+#Acumulado diario de horas de atividades oficiais
+df_diario = df.groupby('MEETING_DATE').sum()[['MEETING_DURATION']].reset_index()
+dates_with_df = df_callendar.merge(df_diario, on='MEETING_DATE', how='left')
+dates_with_df = dates_with_df[(dates_with_df['MEETING_DATE'] <= date.today())]
+df_acumulado = dates_with_df[['MEETING_DATE', 'MEETING_DURATION', 'DIA_UTIL']].fillna(0)
+df_acumulado = pd.concat([df_acumulado, df_acumulado[['MEETING_DURATION', 'DIA_UTIL']].cumsum().add_prefix('CUM_')],axis=1)
+df_acumulado['CUM_DIA_UTIL'] = df_acumulado['CUM_DIA_UTIL'] * 8
+
+acumulado_diario = make_subplots(specs=[[{"secondary_y": True}]])
+acumulado_diario.update_layout(template=template)
+acumulado_diario.add_trace(go.Bar(
+    x=df_acumulado["MEETING_DATE"],
+    y=df_acumulado["CUM_MEETING_DURATION"],
+    name="Presidência"
+), secondary_y=False)
+
+acumulado_diario.add_trace(go.Scatter(
+    x=df_acumulado["MEETING_DATE"],
+    y=df_acumulado["CUM_DIA_UTIL"],
+    name="CLT"
+), secondary_y=False)
+
+acumulado_diario.add_trace(go.Scatter(
+    x=df_acumulado["MEETING_DATE"],
+    y=df_acumulado["CUM_MEETING_DURATION"]/df_acumulado["CUM_DIA_UTIL"],
+    name="Comparativo"
+), secondary_y=True)
+
+# Set x-axis title
+acumulado_diario.update_xaxes(title_text="Data")
+
+# Set y-axes titles
+acumulado_diario.update_yaxes(title_text="Acumulado Horas de Trabalho", secondary_y=False)
+acumulado_diario.update_yaxes(title_text="Horas da Presidencia/CLT", secondary_y=True)
+
 #Examinando os tipos de reuniões que foram realizadas
 tf_idf_vec_smooth = TfidfVectorizer(use_idf=True,  
                         smooth_idf=True,  
@@ -172,11 +209,49 @@ plot_por_similaridade = px.scatter(completed_df, x="DIMENSION_0", y="DIMENSION_1
                  }, 
                  template=template)
 
+#Primeiro compromisso dos dias
+df_primeira_atividade_do_dia = df.groupby('MEETING_DATE').agg({'ROUNDED_BEGIN_HOUR': 'min'}).reset_index()
+df_primeira_atividade_do_dia = df_primeira_atividade_do_dia.groupby(['ROUNDED_BEGIN_HOUR'])['MEETING_DATE'].count().reset_index()
+df_ultima_atividade_do_dia = df.groupby('MEETING_DATE').agg({'ROUNDED_END_HOUR': 'max'}).reset_index()
+df_ultima_atividade_do_dia = df_ultima_atividade_do_dia.groupby(['ROUNDED_END_HOUR'])['MEETING_DATE'].count().reset_index()
+df_primeira_atividade_do_dia['DAY_HOUR'] = df_primeira_atividade_do_dia['ROUNDED_BEGIN_HOUR']
+df_ultima_atividade_do_dia['DAY_HOUR'] = df_ultima_atividade_do_dia['ROUNDED_END_HOUR']
+
+df_comeco_final_dia = df_primeira_atividade_do_dia.merge(df_ultima_atividade_do_dia, on='DAY_HOUR', how='outer').fillna(0)
+
+primeira_ultima_atividade = make_subplots(specs=[[{"secondary_y": False}]])
+primeira_ultima_atividade.update_layout(template=template)
+
+primeira_ultima_atividade.add_trace(go.Bar(
+    x=df_comeco_final_dia["DAY_HOUR"],
+    y=df_comeco_final_dia["MEETING_DATE_x"],
+    name="Horario Inicial"
+), secondary_y=False)
+
+primeira_ultima_atividade.add_trace(go.Bar(
+    x=df_comeco_final_dia["DAY_HOUR"],
+    y=df_comeco_final_dia["MEETING_DATE_y"],
+    name="Horário Final"
+), secondary_y=False)
+
+# Set x-axis title
+primeira_ultima_atividade.update_xaxes(title_text="Hora do Dia")
+# Set y-axes titles
+primeira_ultima_atividade.update_yaxes(title_text="Contagem de Dias", secondary_y=False)
+
 #Média de horas trabalhadas por dia
 media_geral_horas = df.groupby('MEETING_DATE').sum()[['MEETING_DURATION']].mean()
 media_geral = str(math.floor(media_geral_horas[0]))+'h'+str(math.floor((media_geral_horas[0] - math.floor(media_geral_horas[0]))*60))
 media_ultimos_30_dias_horas = df[df['MEETING_DATE'] > (date.today()- timedelta(days=30))].groupby('MEETING_DATE').sum()[['MEETING_DURATION']].mean()
 media_ultimos_30_dias = str(math.floor(media_ultimos_30_dias_horas[0]))+'h'+str(math.floor((media_ultimos_30_dias_horas[0] - math.floor(media_ultimos_30_dias_horas[0]))*60))
+
+#Quantidade de dias úteis sem atividades oficiais
+dates_with_df_null = dates_with_df[(dates_with_df['MEETING_DURATION'].isnull()) & (dates_with_df['DIA_UTIL'] == 1)]
+dias_uteis_sem_atividades_oficiais = len(dates_with_df_null)
+
+#Comparativo de trabalho do Presidente com CLT
+comparativo_percentual = round(df_acumulado.tail(1)["CUM_MEETING_DURATION"]/df_acumulado.tail(1)["CUM_DIA_UTIL"]*100)
+comparativo_percentual = str(int(list(comparativo_percentual)[0])) + "%"
 
 # Initialise the app
 app = dash.Dash(__name__)
@@ -185,7 +260,7 @@ app.config.suppress_callback_exceptions = True
 # Define the app
 app.layout = html.Div(className='row',
     children=[
-        html.Div(className='two columns div-user-controls',
+        html.Div(className='three columns div-user-controls',
                     children=[
                         html.H2('Agenda Presidencial'),
                         dbc.Card([
@@ -199,6 +274,26 @@ app.layout = html.Div(className='row',
                                 ]
                             )
                         ],
+                        style={"width": "30rem"}),
+                        dbc.Card([
+                            dbc.CardHeader("Existem "),
+                            dbc.CardBody(
+                                [
+                                    html.H4(str(dias_uteis_sem_atividades_oficiais) + " dias", className="card-title"),
+                                    html.P("úteis sem atividades oficiais desde a posse", className="card-text")
+                                ]
+                            )
+                        ],
+                        style={"width": "30rem"}),
+                        dbc.Card([
+                            dbc.CardHeader("As atividades oficiais somam"),
+                            dbc.CardBody(
+                                [
+                                    html.H4(comparativo_percentual, className="card-title"),
+                                    html.P("da carga de trabalho de um funcionário CLT", className="card-text")
+                                ]
+                            )
+                        ],
                         style={"width": "30rem"})
                 ]),
         html.Div(className='eight columns div-for-charts bg-grey',
@@ -207,14 +302,22 @@ app.layout = html.Div(className='row',
                             html.H2('Quantidade de atividades oficiais por hora de início'),
                             dcc.Graph(id='agrupado-agenda-diaria', figure = agrupado_agenda_hora_diaria)
                         ]),
-                        html.Div(className='grafico-de-barra-grande', children = [
-                            html.H2('Horas de atividades oficiais por mês'),
-                            dcc.Graph(id='agrupado-agenda-mensal', figure = agrupado_agenda_mensal)
+                        html.Div(className='grafico-de-barra-pequeno', children = [
+                            html.H2('Horário de início e fim dos dias com atividades oficiais'),
+                            dcc.Graph(id='agrupado-agenda-diaria-unica', figure = primeira_ultima_atividade)
                         ]),
                         html.Div(className='grafico-de-barra-grande', children = [
                             html.H2('Horas de atividades oficiais por dia da semana'),
                             dcc.Graph(id='agrupado-agenda-semana', figure = agrupado_agenda_semana)
                         ]),  
+                        html.Div(className='grafico-de-barra-grande', children = [
+                            html.H2('Horas de atividades oficiais por mês'),
+                            dcc.Graph(id='agrupado-agenda-mensal', figure = agrupado_agenda_mensal)
+                        ]),
+                        html.Div(className='grafico-de-barra-grande', children = [
+                            html.H2('Acumulado de horas de atividades oficiais'),
+                            dcc.Graph(id='agrupado-diario', figure = acumulado_diario)
+                        ]),
                         html.Div(className='diagrama-de-bloco', children = [
                             html.H2('Quantidade de atividades oficiais por local'),
                             dcc.Graph(id='agrupado-local', figure = agrupado_agenda_local)
